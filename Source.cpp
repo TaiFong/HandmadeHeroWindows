5e23705181eac0e50671883fc03acc879341c216
TaiFong <codefong.c@gmail.com>
2025-01-26 18:24:46 -0800

    Initial

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+
+int CALLBACK WinMain(
+    _In_  HINSTANCE hInstance,
+    _In_  HINSTANCE hPrevInstance,
+    _In_  LPSTR     lpCmdLine,
+    _In_  int       nCmdShow){
+    
+}
+
+void foo(void){
+    // Function code
+}
